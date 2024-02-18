2434f6a8bb906bd738a28bb15069ec9b18790288
Your Name <you@example.com>
2024-02-18 21:18:01 +0700

    project done

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -54,6 +54,28 @@ public class TicTacToe implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		for(int i = 0; i < 9; i++) {
+			if(e.getSource() == buttons[i]) {
+				if(player1_turn) {
+					if(buttons[i].getText()=="") {
+						buttons[i].setForeground(new Color(255,0,0));
+						buttons[i].setText("X");
+						player1_turn = false;
+						textfield.setText("O turn");
+						check();
+					}
+				}
+				else {
+					if(buttons[i].getText() == "") {
+						buttons[i].setForeground(new Color(0,0,255));
+						buttons[i].setText("O");
+						player1_turn = true;
+						textfield.setText("X turn");
+						check();
+					}
+				}
+			}			
+		}
 	}
 	
 	public void firstTurn() {
@@ -73,14 +95,128 @@ public class TicTacToe implements ActionListener {
 	}
 	
 	public void check() {
-		
+		//Check X win
+		if(
+			(buttons[0].getText() == "X") &&
+			(buttons[1].getText() == "X") &&
+			(buttons[2].getText() == "X")
+		) {
+			xWins(0, 1, 2);
+		}
+		if(
+			(buttons[3].getText() == "X") &&
+			(buttons[4].getText() == "X") &&
+			(buttons[5].getText() == "X")
+		) {
+			xWins(3, 4, 5);
+		}
+		if(
+			(buttons[6].getText() == "X") &&
+			(buttons[7].getText() == "X") &&
+			(buttons[8].getText() == "X")
+		) {
+			xWins(6, 7, 8);
+		}
+		if(
+			(buttons[0].getText() == "X") &&
+			(buttons[3].getText() == "X") &&
+			(buttons[6].getText() == "X")
+		) {
+			xWins(0, 3, 5);
+		}
+		if(
+			(buttons[2].getText() == "X") &&
+			(buttons[5].getText() == "X") &&
+			(buttons[8].getText() == "X")
+		) {
+			xWins(2, 5, 8);
+		}
+		if(
+			(buttons[0].getText() == "X") &&
+			(buttons[4].getText() == "X") &&
+			(buttons[8].getText() == "X")
+		) {
+			xWins(0, 4, 8);
+		}
+		if(
+			(buttons[2].getText() == "X") &&
+			(buttons[4].getText() == "X") &&
+			(buttons[6].getText() == "X")
+		) {
+			xWins(2, 4, 6);
+		}
+
+		//Check O win
+		if(
+			(buttons[0].getText() == "O") &&
+			(buttons[1].getText() == "O") &&
+			(buttons[2].getText() == "O")
+		) {
+			oWins(0, 1, 2);
+		}
+		if(
+			(buttons[3].getText() == "O") &&
+			(buttons[4].getText() == "O") &&
+			(buttons[5].getText() == "O")
+		) {
+			oWins(3, 4, 5);
+		}
+		if(
+			(buttons[6].getText() == "O") &&
+			(buttons[7].getText() == "O") &&
+			(buttons[8].getText() == "O")
+		) {
+			oWins(6, 7, 8);
+		}
+		if(
+			(buttons[0].getText() == "O") &&
+			(buttons[3].getText() == "O") &&
+			(buttons[6].getText() == "O")
+		) {
+			oWins(0, 3, 5);
+		}
+		if(
+			(buttons[2].getText() == "O") &&
+			(buttons[5].getText() == "O") &&
+			(buttons[8].getText() == "O")
+		) {
+			oWins(2, 5, 8);
+		}
+		if(
+			(buttons[0].getText() == "O") &&
+			(buttons[4].getText() == "O") &&
+			(buttons[8].getText() == "O")
+		) {
+			oWins(0, 4, 8);
+		}
+		if(
+			(buttons[2].getText() == "O") &&
+			(buttons[4].getText() == "O") &&
+			(buttons[6].getText() == "O")
+		) {
+			oWins(2, 4, 6);
+		}
 	}
 	
 	public void xWins(int a, int b, int c) {
-		
+		buttons[a].setBackground(Color.GREEN);
+		buttons[b].setBackground(Color.GREEN);
+		buttons[c].setBackground(Color.GREEN);
+
+		for(int i = 0; i < 9; i++) {
+			buttons[i].setEnabled(false);
+		}
+		textfield.setText("X win");
 	}
 	
 	public void oWins(int a, int b, int c) {
-		
+		buttons[a].setBackground(Color.GREEN);
+		buttons[b].setBackground(Color.GREEN);
+		buttons[c].setBackground(Color.GREEN);
+
+		for(int i = 0; i < 9; i++) {
+			buttons[i].setEnabled(false);
+		}
+		textfield.setText("O win");
 	}
 }
